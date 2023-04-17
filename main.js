b6674bdfdc34d85e614de930b3d77c382a2fd55f
seiflabs <130853462+seiflabs@users.noreply.github.com>
2023-04-18 01:14:06 +0200

    Update main.js

--- main.js
+++ main.js
@@ -7,7 +7,7 @@ http.createServer(function (request, response) {
    response.writeHead(200, {'Content-Type': 'text/plain'});
    
    // Send the response body as "Hello World"
-   response.end('Test trigger\n');
+   response.end('Testing trigger\n');
 }).listen(8081);
 
 // Console will print the message
