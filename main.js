9fc67ebe0849336a319de162342354e6a2bd26ad
root <root@ip-172-31-44-186.eu-west-3.compute.internal>
2023-04-17 10:36:32 +0000

    Ajout branche Rodrigue BONBON

--- main.js
+++ main.js
@@ -7,7 +7,7 @@ http.createServer(function (request, response) {
    response.writeHead(200, {'Content-Type': 'text/plain'});
    
    // Send the response body as "Hello World"
-   response.end('Romain DEVAUX\n');
+   response.end('Rodrigue BONBON\n');
 }).listen(8081);
 
 // Console will print the message
